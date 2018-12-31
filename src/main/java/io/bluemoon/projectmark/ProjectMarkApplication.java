918fcbf4de9de4a057502545ff4d9639d7f4b150
liquidjoo <liquidjoo@gmail.com>
2018-12-31 23:35:23 +0900

    project mark init

---
+++ src/main/java/io/bluemoon/projectmark/ProjectMarkApplication.java
@@ -0,0 +1,14 @@
+package io.bluemoon.projectmark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMarkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectMarkApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/io/bluemoon/projectmark/ProjectMarkApplicationTests.java
@@ -0,0 +1,17 @@
+package io.bluemoon.projectmark;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjectMarkApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
