c2e889ae4a408960344024d1106126469d83064d
liquidjoo <liquidjoo@gmail.com>
2019-01-02 23:48:11 +0900

    contents module init

---
+++ contents/src/main/java/io/bluemoon/contents/ContentsApplication.java
@@ -0,0 +1,14 @@
+package io.bluemoon.contents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContentsApplication.class, args);
+    }
+
+}
+
