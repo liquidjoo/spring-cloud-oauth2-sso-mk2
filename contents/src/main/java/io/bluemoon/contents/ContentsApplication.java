046310d1c9421406af44a7575a33bf10c5bae52d
liquidjoo <liquidjoo@gmail.com>
2019-01-09 23:00:03 +0900

    RestTemplate 빈등록, Eureka discovery

--- contents/src/main/java/io/bluemoon/contents/ContentsApplication.java
+++ contents/src/main/java/io/bluemoon/contents/ContentsApplication.java
@@ -2,9 +2,22 @@ package io.bluemoon.contents;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
+@EnableEurekaClient
+@EnableCircuitBreaker
 @SpringBootApplication
 public class ContentsApplication {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ContentsApplication.class, args);
