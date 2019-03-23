5f6fe215ccfa8a75592d64e04c590bdb1fc94851
liquidjoo <liquidjoo@gmail.com>
2019-03-24 00:41:54 +0900

    aws stack auto option check

--- queue/src/main/java/io/bluemoon/queue/QueueApplication.java
+++ queue/src/main/java/io/bluemoon/queue/QueueApplication.java
@@ -1,7 +1,12 @@
 package io.bluemoon.queue;
 
+import com.amazonaws.services.sqs.AmazonSQSAsync;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.aws.messaging.config.annotation.EnableSqs;
+import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class QueueApplication {
@@ -9,5 +14,10 @@ public class QueueApplication {
     public static void main(String[] args) {
         SpringApplication.run(QueueApplication.class, args);
     }
+
+//    @Bean
+//    public QueueMessagingTemplate queueMessagingTemplate(AmazonSQSAsync amazonSQSAsync) {
+//        return new QueueMessagingTemplate(amazonSQSAsync);
+//    }
 
 }
