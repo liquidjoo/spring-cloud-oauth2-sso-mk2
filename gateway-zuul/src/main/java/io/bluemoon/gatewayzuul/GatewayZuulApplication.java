eeb860cd4e3f3bb7fee3df6c28388ad6f33073f9
tom <liquidme@uneedcomm.com>
2019-05-16 18:30:57 +0900

    resource server gateway delete

---
+++ gateway-zuul/src/main/java/io/bluemoon/gatewayzuul/GatewayZuulApplication.java
@@ -0,0 +1,13 @@
+package io.bluemoon.gatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayZuulApplication.class, args);
+    }
+
+}
