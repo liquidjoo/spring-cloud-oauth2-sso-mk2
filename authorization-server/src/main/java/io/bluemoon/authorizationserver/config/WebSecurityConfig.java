bc6f7b61d36335c6443f5ab8366d9bc36c9923f5
tom <liquidme@uneedcomm.com>
2019-05-16 18:30:30 +0900

    resource server delete

--- authorization-server/src/main/java/io/bluemoon/authorizationserver/config/WebSecurityConfig.java
+++ authorization-server/src/main/java/io/bluemoon/authorizationserver/config/WebSecurityConfig.java
@@ -12,10 +12,8 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @Configuration
-@EnableResourceServer
 @Order(SecurityProperties.BASIC_AUTH_ORDER - 6)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
