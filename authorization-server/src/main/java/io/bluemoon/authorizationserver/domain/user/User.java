36d466406e9c165230ec33022cf5e14864d1bf5c
liquidjoo <liquidjoo@gmail.com>
2019-05-21 17:22:21 +0900

    model generation type auto -> identity

--- authorization-server/src/main/java/io/bluemoon/authorizationserver/domain/user/User.java
+++ authorization-server/src/main/java/io/bluemoon/authorizationserver/domain/user/User.java
@@ -9,7 +9,7 @@ import java.util.Date;
 @Data
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(length = 20, nullable = false, unique = true)
